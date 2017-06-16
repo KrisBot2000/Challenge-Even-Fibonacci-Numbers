1ca665b7cf97b2142f71e7c2f611e3bf76a56095
Kristin Treder <stuffs@Kristins-MacBook.local>
2017-06-16 11:08:10 -1000

    first two parts pass

--- even_fib_nums.js
+++ even_fib_nums.js
@@ -8,19 +8,67 @@
 function _sumFibs( maxFibValue ) {
   var sum = 0;
 
-  // do your work here
+
+
+
+  let arrFib=[0,1];
+  let arrFibEven=[];
+
+  //create arrays of Fib numbers and even Fib numbers
+
+  for (i=2; i<10000000; i++){
+    arrFib[i] = arrFib[i-2] + arrFib[i-1];
+    //console.log(arrFib[i]);
+    arrFib.push(arrFib[i]);
+    //console.log(arrFib);
+    if (arrFib[i]%2===0){
+      arrFibEven.push(arrFib[i]);
+      //console.log(arrFibEven);
+    }
+  }
+
+  //iterate through array of even Fib numbers and sum up numbers less than maxFibValue
+
+  for(i=0; i<10000002; i++){
+    if (arrFibEven[i] <= maxFibValue){
+      sum += arrFibEven[i];
+    }
+
+
+
+
+  }
 
   return sum;
 }
+
+
+
 
 // bonus round
 function _highestFibonacciNumber (maxFibValue){
   var highest = 0;
 
-  //define your base case, validate your input
+  //let arrFib=[0,1];
+  //let arrFibEven=[];
 
 
-  //do your work here
+  // //define your base case, validate your input
+  // for (i=2; i<10000000; i++){
+  //   arrFib[i] = arrFib[i-2] + arrFib[i-1];
+  //   //console.log(arrFib[i]);
+  //   arrFib.push(arrFib[i]);
+  //   //console.log(arrFib);
+  //   if (arrFib[i]%2===0){
+  //     arrFibEven.push(arrFib[i]);
+  //     //console.log(arrFibEven);
+  //   }
+  // }
+
+  // for(i=0; i<10000002; i++){
+  //   if (arrFibEven[i] < maxFibValue){
+  //     highest = arrFibEven[i];
+  //   }
 
   return highest;
 };
